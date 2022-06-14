2302af68e939f6b91c1ac99881d889df2ac4f492
Legimaria <100758111+Legimaria@users.noreply.github.com>
2022-06-14 23:57:21 +0100

    Update home.blade.php

--- MSIB_Laravel_Projek/resources/views/content-landing/home/home.blade.php
+++ MSIB_Laravel_Projek/resources/views/content-landing/home/home.blade.php
@@ -9,7 +9,7 @@
                 <!-- Slider Item -->
                 <div class="owl-item">
                     <div class="home_slider_background"
-                        style="background-image:url({{ asset('assets/landing/images/home_background_1.jpg') }})">
+                        style="background-image:url({{ asset('assets/landing/images/botol.jpg') }})">
                     </div>
                     <div class="home_content">
                         <div class="container">
@@ -17,12 +17,10 @@
                                 <div class="col">
                                     <div class="home_content_inner">
                                         <div class="home_title">
-                                            <h1>Medicine made with care</h1>
+                                            <h1>Lakukan Vaksin untuk Kesehatan Keluarga yang Lebih Baik</h1>
                                         </div>
                                         <div class="home_text">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                                                tempus vestibulum mauris quis aliquam. Integer accumsan sodales
-                                                odio, id tempus velit ullamcorper id. Quisque at erat eu.</p>
+                                            <p>Setiap orang berhak mendapatkan vaksin berkualitas </p>
                                         </div>
                                         <div class="button home_button">
                                             <a href="#">read more</a>
@@ -140,9 +138,8 @@
                             </div>
                         </div>
                         <div class="box_title">Appointments</div>
-                        <div class="box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Curabitur ante leo, finibus quis est ut, tempor tincidunt ipsum. Nam cons equat semper
-                            sollicitudin.</div>
+                        <div class="box_text">Jika ada perencanaan pertemuan pribadi, silakan hubungi Helpdesk berikut :
+0895 3768 60088</div>
                     </div>
                 </div>
 
@@ -156,8 +153,7 @@
                         </div>
                         <div class="box_title">Emergency Cases</div>
                         <div class="box_phone">+56 273 45678 235</div>
-                        <div class="box_emergency_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Curabitur ante leo.</div>
+                        <div class="box_emergency_text">Informasi lebih lanjut hubungi: Contact Center HALO BPOM di nomor telepon 1-500-533, SMS 0-8121-9999-533, WhatsApp 0811-9181 533, email halobpom@pom.go.id,  twitter@BPOM_RI, atau Unit Layanan Pengaduan Konsumen (ULPK) Balai Besar/Balai POM di seluruh Indonesia</div>
                     </div>
                 </div>
 
@@ -175,16 +171,10 @@
                 <div class="col-lg-7">
                     <div class="about_content">
                         <div class="section_title">
-                            <h2>A great medical team to help your needs</h2>
+                            <h2>Tim medis yang hebat untuk membantu kebutuhan Anda</h2>
                         </div>
                         <div class="about_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ante leo, finibus
-                                quis est ut, tempor tincidunt ipsum. Nam consequat semper sollicitudin. Aliquam nec
-                                dapibus massa. Pellen tesque in luctus ex. Praesent luctus erat sit amet tortor
-                                aliquam bibendum. Nulla ut molestie augue, scelerisque consectetur quam. Dolor sit
-                                amet, consectetur adipiscing elit. Cura bitur ante leo, finibus quis est ut, tempor
-                                tincidunt ipsum. Nam consequat semper sollicitudin. Aliquam nec dapibus massa.
-                                Pellentesque in luctus ex.</p>
+                            <p>Setelah melakukan vaksin, akan dilakukan observasi selama 15 menit. Jika dalam 15 menit tersebut terdapat keluhan, maka akan ditangani oleh tim medis yang bertugas di lokasi vaksinasi. Jika keluhan baru dirasakan ketika anak pulang, maka dapat melaporkan ke puskesmas atau dapat menghubungi kontak yang ada di kartu vaksin.</p>
                         </div>
                         <div class="button about_button">
                             <a href="#">read more</a>
@@ -194,7 +184,7 @@
 
                 <!-- About Image -->
                 <div class="col-lg-5">
-                    <div class="about_image"><img src="{{ asset('assets/landing/images/about.png') }}" alt=""></div>
+                    <div class="about_image"><img src="{{ asset('assets/landing/images/medis.jpg') }}" alt=""></div>
                 </div>
             </div>
         </div>
@@ -218,7 +208,7 @@
                 <!-- Department -->
                 <div class="col-lg-3 col-md-6 dept_col">
                     <div class="dept">
-                        <div class="dept_image"><img src="{{ asset('assets/landing/images/dept_1.jpg') }}" alt="">
+                        <div class="dept_image"><img src="{{ asset('assets/landing/images/pendaftaran.jpg') }}" alt="">
                         </div>
                         <div class="dept_content text-center">
                             <div class="dept_title">plastic surgery</div>
@@ -230,7 +220,7 @@
                 <!-- Department -->
                 <div class="col-lg-3 col-md-6 dept_col">
                     <div class="dept">
-                        <div class="dept_image"><img src="{{ asset('assets/landing/images/dept_2.jpg') }}" alt="">
+                        <div class="dept_image"><img src="{{ asset('assets/landing/images/skrinning.jpg') }}" alt="">
                         </div>
                         <div class="dept_content text-center">
                             <div class="dept_title">gastroenterology</div>
@@ -242,7 +232,7 @@
                 <!-- Department -->
                 <div class="col-lg-3 col-md-6 dept_col">
                     <div class="dept">
-                        <div class="dept_image"><img src="{{ asset('assets/landing/images/dept_3.jpg') }}" alt="">
+                        <div class="dept_image"><img src="{{ asset('assets/landing/images/vaksinasi.jpg') }}" alt="">
                         </div>
                         <div class="dept_content text-center">
                             <div class="dept_title">dentistry</div>
@@ -255,9 +245,11 @@
                 <div class="col-lg-3 col-md-6 dept_col">
                     <div class="dept">
                         <div class="dept_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ante leo, finibus
-                                quis est ut, tempor tincidunt ipsum. Nam consequat semper sollicitudin. Aliquam nec
-                                dap ibus massa. Pellen tesque in luctus ex.</p>
+                            <p>Meski mendapat vaksin, petugas akan tetap menjaga jarak, menggunakan APD.
+                                Meja 1 Pendaftaran,
+                                Meja 2 Skrinning,
+                                Meja 3 Vaksinasi,
+                                Meja 4 Pendataan dan Observasi.</p>
                         </div>
                         <div class="button dept_button"><a href="#">read more</a></div>
                     </div>
@@ -288,8 +280,7 @@
                             </div>
                             <div class="service_title trans_200">Free Checkups</div>
                             <div class="service_text">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ante leo,
-                                    finibus quis est ut, tempor tincidunt ipsum. Nam consequat semper sollicitudin.
+                                <p>Silahkan berkunjung ke layanan kami untuk memeriksa apakah anda sudah bisa melakukan vaksinasi dosis berikutnya.
                                 </p>
                             </div>
                         </div>
@@ -393,10 +384,10 @@
             <div class="row">
                 <div class="col">
                     <div class="cta_content text-center">
-                        <h2>Need a personal health plan?</h2>
-                        <p>Duis massa massa, mollis vel ullamcorper quis, finibus et urna. Aliquam ac eleifend
-                            metus. Ut sollicitudin risus ex</p>
-                        <div class="button cta_button"><a href="#">request a plan</a></div>
+                        <h2>Apakah anda membutuhkan bantuan?</h2>
+                        <p>Jika pertanyaan tidak tersedia di FAQ, silakan hubungi Helpdesk berikut :
+0895 3768 60088</p>
+                        <div class="button cta_button"><a href="#">Ajukan Permohonan</a></div>
                     </div>
                 </div>
             </div>
