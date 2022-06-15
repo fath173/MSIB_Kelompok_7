fb0cc5d7bd1eefcc9a9d1b21aad085a5de38533a
cahyaaprilia <82190636+cahyaaprilia@users.noreply.github.com>
2022-06-15 16:23:39 +0700

    Update header.blade.php

--- MSIB_Laravel_Projek/resources/views/content-landing/header.blade.php
+++ MSIB_Laravel_Projek/resources/views/content-landing/header.blade.php
@@ -1,12 +1,12 @@
 <div class="home">
     <div class="home_background parallax-window" data-parallax="scroll"
-        data-image-src="{{ asset('assets/landing/images/about.jpg') }}" data-speed="0.8"></div>
+        data-image-src="{{ asset('assets/landing/images/vaksin3.jpg') }}" data-speed="0.8"></div>
     <div class="home_container">
         <div class="container">
             <div class="row">
                 <div class="col">
                     <div class="home_content">
-                        <div class="home_title">About <span>CareMed</span></div>
+                        <div class="home_title">About <span>VaksinMed</span></div>
                         <div class="breadcrumbs">
                             <ul>
                                 <li><a href="{{ route('landing-home') }}">Home</a></li>
