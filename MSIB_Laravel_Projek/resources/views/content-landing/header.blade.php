af522f786760b3cc9b450c18c670a49b2479bfc1
Fathor Rahman <fathorrahman2357@gmail.com>
2022-06-26 12:27:31 +0700

    Summary

--- MSIB_Laravel_Projek/resources/views/content-landing/header.blade.php
+++ MSIB_Laravel_Projek/resources/views/content-landing/header.blade.php
@@ -1,6 +1,7 @@
 <div class="home">
     <div class="home_background parallax-window" data-parallax="scroll"
-        data-image-src="{{ asset('assets/landing/images/vaksin3.jpg') }}" data-speed="0.8"></div>
+        data-image-src="{{ asset('assets/landing/images/vaksin3.jpg') }}" data-speed="0.8">
+    </div>
     <div class="home_container">
         <div class="container">
             <div class="row">
